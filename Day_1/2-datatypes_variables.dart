bf6cc7ea72e33a02b7d70c5bf49f7136cab9bb65
Mike15254 <mike8max10@gmail.com>
2022-10-18 20:46:03 +0300

    second commit

---
+++ Day_1/2-datatypes_variables.dart
@@ -0,0 +1,19 @@
+void main() {
+  // interger declaration
+  int age = 10;
+  // or
+  int ages = 10;
+
+  // double declaration
+  double mini = 9.4;
+
+  // string declaration
+  String name = "Mike";
+  // or
+  var names = "Mike";
+
+  // boolen declaration
+  bool isNew = true;
+  // or
+  var islike = true;
+}
---
+++ Day_1/3-strings.dart
@@ -0,0 +1,20 @@
+void main() {
+  // literals
+  var isCool = true;
+  2;
+  "John";
+  4.5;
+
+  // Various ways to define string literals in dart
+  String s1 = 'Single';
+  String s2 = "Double";
+  String s3 = 'It\'s easy';
+  String s5 = 'this is my stroy,,,,,,,,,'
+      'am from kenya,,,,,,,,,,,';
+
+  // String interpolation
+  String name = "mike";
+
+  print("My name is $name");
+  print("The number of characters in string $name is  $name.length");
+}
